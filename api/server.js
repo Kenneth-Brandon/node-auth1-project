79449bfdf37c2fd5d226e01bd00093c777678144
unknown <ken.brandon@aol.com>
2020-06-03 17:51:59 -0700

    cleanup in index and server files and created and exported the users router

--- api/server.js
+++ api/server.js
@@ -2,10 +2,18 @@ const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
 
+const usersRouter = require('../users/users-router');
+
 const server = express();
 
-server.use(express.json());
 server.use(helmet());
+server.use(express.json());
 server.use(cors());
+
+server.use('/api/users', usersRouter);
+
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'API is up!' });
+});
 
 module.exports = server;
--- index.js
+++ index.js
@@ -2,10 +2,6 @@ const app = require('./api/server');
 
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'API is up!' });
-});
-
 app.listen(PORT, () => {
   console.log(`***Listening on port ${PORT} ***`);
 });
