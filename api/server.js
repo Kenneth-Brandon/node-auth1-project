7684702486ff194203cf3013e590b28ff8868e0a
unknown <ken.brandon@aol.com>
2020-06-03 18:34:38 -0700

    debugged and got server running and passed users-router endpoint through postman

--- api/server.js
+++ api/server.js
@@ -12,7 +12,7 @@ server.use(cors());
 
 server.use('/api/users', usersRouter);
 
-app.get('/', (req, res) => {
+server.get('/', (req, res) => {
   res.status(200).json({ message: 'API is up!' });
 });
 
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const app = require('./api/server');
+const server = require('./api/server');
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`***Listening on port ${PORT} ***`);
 });
