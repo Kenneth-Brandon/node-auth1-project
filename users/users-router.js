6893327f18f21f4be47fc1e12e086a89d543d552
unknown <ken.brandon@aol.com>
2020-06-03 18:15:02 -0700

    finished users-router

--- users/users-router.js
+++ users/users-router.js
@@ -1,7 +1,13 @@
-const express = require('express');
-
 const router = express.Router();
 
-router.post('/', async (req, res, next) => {});
+const Users = require('./users-model');
+
+router.get('/', async (req, res) => {
+  Users.find()
+    .then((users) => {
+      res.json(users);
+    })
+    .catch((err) => res.send(err));
+});
 
 module.exports = router;
